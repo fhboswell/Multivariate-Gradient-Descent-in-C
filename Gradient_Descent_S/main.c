6247b726456c7770f86d720cfc739aa01218b6d1
fhboswell <franklin.henry.boswell@gmail.com>
2017-08-14 16:20:31 -0400

    Initial Commit

---
+++ Gradient_Descent_S/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Gradient_Descent_S
+//
+//  Created by Henry Boswell on 8/14/17.
+//  Copyright © 2017 Henry Boswell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
